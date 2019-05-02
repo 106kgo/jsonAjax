2c63f010dd14e2293cbf1be7fa95b7df01f67942
Gabriel Olmos <gabe_olmos@yahoo.com>
2019-05-02 16:05:54 -0700

    85% done with exercise

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,21 @@
-let ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-ourRequest.onload = function(){
-let ourData = JSON.parse(ourRequest.responseText);
-console.log(ourData[0]);
-
-};
-ourRequest.send();
+let animalContainer = document.getElementById("animal-info");
+// grabbing the button on the html
+let btn = document.getElementById("btn");
+// activates when button is clicked
+btn.addEventListener("click", function(){
+    // AJAX call getting info from another URL
+    let ourRequest = new XMLHttpRequest();
+    // location to URL
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    // grabbing specific data from URL
+    ourRequest.onload = function(){
+        // Pulling data and letting computer know to read in JSON format
+        let ourData = JSON.parse(ourRequest.responseText);
+        addHTML(ourData);
+    };
+    ourRequest.send();
+});
+// adding HTML the page
+function addHTML(data){
+animalContainer.insertAdjacentHTML('beforeend', 'testing 123');
+}
