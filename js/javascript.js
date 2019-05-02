42b764c9057d03bfbc46d99010b2b6c07241e115
Gabriel Olmos <gabe_olmos@yahoo.com>
2019-05-02 13:33:52 -0700

    almost done with exercise

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+let ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+ourRequest.onload = function(){
+let ourData = JSON.parse(ourRequest.responseText);
+console.log(ourData[0]);
+
+};
+ourRequest.send();
